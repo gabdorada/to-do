a6b72d345607a38508fc6bb72b301fa4ddf2417f
gabdorada <gabdorada@gmail.com>
2026-04-19 17:35:17 -0300

    feat: initial project structure and main menu loop

---
+++ main.py
@@ -0,0 +1,15 @@
+# Define function called main():
+def main ():
+    # Creat a list, armazenar nossas tarefas
+    tasks = []
+while True:
+    print("=== To-Do List ===")
+    print("1. Add Task")
+    print("2. Show Tasks")
+    print("3. Mark Task as Done")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    # The user is prompted to enter their choice.
+    
